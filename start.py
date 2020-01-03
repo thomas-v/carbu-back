a5b35f211b7db16fc86e6012834d991de2f9d43c
Thomas Vitiello <tvitiello@vps706209.ovh.net>
2020-01-03 22:53:35 +0100

    recuperation des infos des stations d un cp

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 import bottle
 from pymongo import MongoClient
+from datetime import datetime
+import json
 
 app = application = bottle.Bottle() 
 
@@ -7,9 +9,16 @@ app = application = bottle.Bottle()
 client = MongoClient('localhost', 27017)
 db = client.carb
 
-@app.route('/')
-def toto():
-    #print(db.pdv.find())
-    return "Test"
+@app.route('/stations/<cp>')
+def getStationsByPostCode(cp):
+    
+    # current date
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    # get stations by postCode 
+    stations = db.pdv.find({'cp' : cp}, {'latitude' : 1, 'longitude' : 1, today : 1})
+    stations_list = [({'latitude' : station['latitude'], 'longitude' : station['longitude'], 'carburants' : station[today]}) for station in stations]
+
+    return json.dumps(stations_list)
 
 bottle.run(app, host='localhost', port=7001, reloader=True)
