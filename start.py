0fadedcb6fdeaed3bbd7fd27c322325676a2e2f3
Thomas Vitiello <tvitiello@vps706209.ovh.net>
2020-01-03 21:22:02 +0100

    connexion au mongodb

--- start.py
+++ start.py
@@ -1,10 +1,15 @@
 import bottle
-import pymongo
+from pymongo import MongoClient
 
 app = application = bottle.Bottle() 
 
+# mongodb connexion
+client = MongoClient('localhost', 27017)
+db = client.carb
+
 @app.route('/')
 def toto():
+    #print(db.pdv.find())
     return "Test"
 
 bottle.run(app, host='localhost', port=7001, reloader=True)
