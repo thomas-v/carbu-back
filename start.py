2ae78892e0d6a1e4fd79f35e454018aa9e0c3cc0
Thomas Vitiello <tvitiello@vps706209.ovh.net>
2020-01-25 23:42:13 +0100

    recherche by cp

--- start.py
+++ start.py
@@ -9,14 +9,23 @@ app = application = bottle.Bottle()
 client = MongoClient('localhost', 27017)
 db = client.carb
 
-@app.route('/stations/<dpt>')
+@app.route('/stationsByDpt/<dpt>')
 def getStationsByDpt(dpt):
 
-    # get stations by postCode 
+    # get stations by dpt 
     stations = db.stations.aggregate([{'$project' : {'adresse' : 1, 'ville' : 1, 'cp' : 1, 'carburants' : 1, 'latitude' : 1, 'longitude' : 1, 'dpt' : {'$substr' : ['$cp', 0, 2]}}}, {'$match' : {'dpt' : dpt}}])
     
     stations_list = [({'adresse' : station['adresse'], 'ville' : station['ville'], 'cp' : station['cp'], 'latitude' : station['latitude'], 'longitude' : station['longitude'], 'carburants' : station['carburants']}) for station in stations]
 
     return json.dumps(stations_list)
+
+@app.route('/stationsByPostCode/<postCode>')
+def getStationsByPostCode(postCode):
+
+    # get stations by postCode 
+    station = db.stations.find_one({'cp' : postCode}, {'latitude' : 1, 'longitude' : 1, '_id' : 0});
+    print(station)
+
+    return json.dumps(station)
 
 bottle.run(app, host='localhost', port=7001, reloader=True)
